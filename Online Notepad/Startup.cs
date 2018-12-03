a4df92413c860ebe86a44356686e15e24cefbd9f
Sergio Esteves <saergio.esteves@gmail.com>
2018-12-03 21:50:20 +0000

    Add project files.

---
+++ Online Notepad/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Notepad.Startup))]
+namespace Online_Notepad
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
